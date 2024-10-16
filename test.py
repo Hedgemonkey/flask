69d73ccf72db180691a85e6130345d1b2f5a9c9a
Hedgemonkey <hedgemonkey@gmail.com>
2024-10-16 19:28:03 +0100

    Add basic flask framework

---
+++ test.py
@@ -0,0 +1,9 @@
+print("Starting script execution...")
+
+try:
+    # ... your script code here ...
+    my_variable = "Hello, world!"
+    print(my_variable) 
+
+except Exception as e:
+    print(f"An error occurred: {e}")
